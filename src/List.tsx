63ab4d71ff1b29fabc87b0628a9d36237dd65357
Christoph Nascone <chris@unlock-protocol.com>
2019-11-09 15:54:50 -0500

    Flesh out the list a bit

--- src/List.tsx
+++ src/List.tsx
@@ -5,14 +5,45 @@ export default function List() {
   return (
     <div>
       <Nav />
+      <ListContents balances={[]} />
     </div>
   )
 }
 
-interface ListContentsProps {
-  
+interface Balance {
+  kind: 'debit' | 'credit'
+  otherParty: string
+  amount: number
 }
 
-function ListContents() {
-  
+interface ListContentsProps {
+  balances: Balance[]
+}
+
+function ListContents({ balances }: ListContentsProps) {
+  return (
+    <ul>
+      {balances.map(balance => {
+        return (
+          <li key={balance.otherParty + '-' + balance.kind}>
+            <ListItem balance={balance} />
+          </li>
+        );
+      })}
+    </ul>
+  )
+}
+
+
+interface ListItemProps {
+  balance: Balance
+}
+function ListItem({ balance }: ListItemProps) {
+  return (
+    <ul>
+      <li>{balance.kind}</li>
+      <li>{balance.otherParty}</li>
+      <li>{balance.amount}</li>
+    </ul>
+  )
 }
