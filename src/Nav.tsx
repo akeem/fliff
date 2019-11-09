3dc62de6178c6f40eb78bd2ab8235dd1471ebb30
Christoph Nascone <chris@unlock-protocol.com>
2019-11-09 14:17:13 -0500

    Setup

---
+++ src/Nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'mustard-ui'
+
+export const Nav = () => (
+  <nav>
+    <div className="nav-container">
+      <div className="nav-logo">
+        <a href="/">Fliff</a>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Nav
