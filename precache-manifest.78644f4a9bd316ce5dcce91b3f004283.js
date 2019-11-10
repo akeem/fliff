54c3d6f645592d604c3a02eabc6f8febdabf8f1a
Christoph Nascone <chris@unlock-protocol.com>
2019-11-09 23:58:37 -0500

    Updates

---
+++ precache-manifest.78644f4a9bd316ce5dcce91b3f004283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6b64d3c063d562359b847bac21c96b",
+    "url": "/fliff/index.html"
+  },
+  {
+    "revision": "5fdbac1329a83e598fbc",
+    "url": "/fliff/static/css/2.1bfb5f9e.chunk.css"
+  },
+  {
+    "revision": "e7374cefa608c197ff73",
+    "url": "/fliff/static/css/main.9d4d29d5.chunk.css"
+  },
+  {
+    "revision": "5fdbac1329a83e598fbc",
+    "url": "/fliff/static/js/2.8a63abf2.chunk.js"
+  },
+  {
+    "revision": "e7374cefa608c197ff73",
+    "url": "/fliff/static/js/main.a372110d.chunk.js"
+  },
+  {
+    "revision": "d2d3a233659692a87f6b",
+    "url": "/fliff/static/js/runtime-main.e8615fa0.js"
+  },
+  {
+    "revision": "68ab0e724bedcfb22d114c7a6398668c",
+    "url": "/fliff/static/media/fliff-logomark.68ab0e72.svg"
+  }
+]);
