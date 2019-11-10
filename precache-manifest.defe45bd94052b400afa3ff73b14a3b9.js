48f67db164f742749e1f68c16578b4dec3fedc21
Christoph Nascone <chris@unlock-protocol.com>
2019-11-10 00:43:17 -0500

    Updates

---
+++ precache-manifest.defe45bd94052b400afa3ff73b14a3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2134c8f8968353b58d0827c78a70fe50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fdbac1329a83e598fbc",
+    "url": "/static/css/2.1bfb5f9e.chunk.css"
+  },
+  {
+    "revision": "972f2a3e7f32e01b4e4d",
+    "url": "/static/css/main.9d4d29d5.chunk.css"
+  },
+  {
+    "revision": "5fdbac1329a83e598fbc",
+    "url": "/static/js/2.8a63abf2.chunk.js"
+  },
+  {
+    "revision": "972f2a3e7f32e01b4e4d",
+    "url": "/static/js/main.f98b6626.chunk.js"
+  },
+  {
+    "revision": "52d124fea91ce57d8114",
+    "url": "/static/js/runtime-main.19612134.js"
+  },
+  {
+    "revision": "68ab0e724bedcfb22d114c7a6398668c",
+    "url": "/static/media/fliff-logomark.68ab0e72.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fliff/precache-manifest.01845281d3a3f3f3a5d48eb676241d18.js"
+  "/precache-manifest.defe45bd94052b400afa3ff73b14a3b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fliff/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
